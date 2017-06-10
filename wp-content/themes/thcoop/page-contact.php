84772064a7a93ea37025e30a34ee94df11932896
kbesse67 <kevin.besse.67@gmail.com>
2017-06-11 00:01:29 +0200

    4- Mise en place du Contact
    Sans le formulaire bien sûr

--- wp-content/themes/thcoop/page-contact.php
+++ wp-content/themes/thcoop/page-contact.php
@@ -4,21 +4,19 @@ Template Name: Contact
 */
 get_header(); ?>
 
-<div class="row expanded">
-
-
+<div class="row expanded mapgoogle">
+<iframe class="themap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2639.878539238785!2d7.787289615664319!3d48.57387507926071!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4796c92714ce0199%3A0x6c9ef56f2525d737!2sRue+de+la+Coop%C3%A9rative%2C+67000+Strasbourg!5e0!3m2!1sfr!2sfr!4v1497126306267" width="1980" height="550" frameborder="0" style="border:0   " allowfullscreen></iframe>
 </div>
 
-<div class="coordonees">
-
-	<h2>Nos coordonés</h2>
-	<h4>Bureau de la Coop</h4>
-	<h4>3, Place Broglie</h4>
-	<h4>6700 Strasbourg</h4>
+<div class="coordonnees">
+	<h2 class="text-center">Nos coordonnées</h2>
+	<h4 class="text-center">Bureau de la Coop</h4>
+	<h4 class="text-center">3, Place Broglie</h4>
+	<h4 class="text-center">6700 Strasbourg</h4>
 </div>
 
 
-<div class="row">
+<div class="row theformulaire">
 	<div class="large-4 medium-12 small-12 columns">
 		<h2>Vous souhaitez nous proposer un projet ?</h2>
 		<p>Vous êtes un investisseur, un artisan, un designer, graphiste, un artiste ? Ou vous disposez d'un projet innovant ?
