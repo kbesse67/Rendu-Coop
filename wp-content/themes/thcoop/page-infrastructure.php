735d2c2e92547b19b0545a115399a66a0de9c381
kbesse67 <kevin.besse.67@gmail.com>
2017-06-11 13:51:57 +0200

    5- Page contact + Infrastructure HTML CSS PHP

--- wp-content/themes/thcoop/page-infrastructure.php
+++ wp-content/themes/thcoop/page-infrastructure.php
@@ -1,19 +1,32 @@
 <?php
 /*
-Template Name: Contact
+Template Name: Infrastructure
 */
+
 get_header(); ?>
 
-<div class="row expanded mapgoogle">
-<iframe class="themap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2639.878539238785!2d7.787289615664319!3d48.57387507926071!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4796c92714ce0199%3A0x6c9ef56f2525d737!2sRue+de+la+Coop%C3%A9rative%2C+67000+Strasbourg!5e0!3m2!1sfr!2sfr!4v1497126306267" width="1980" height="550" frameborder="0" style="border:0   " allowfullscreen></iframe>
-</div>
-
-<div class="coordonnees">
-	<h2 class="text-center">Nos coordonnées</h2>
-	<h4 class="text-center">Bureau de la Coop</h4>
-	<h4 class="text-center">3, Place Broglie</h4>
-	<h4 class="text-center">6700 Strasbourg</h4>
-</div>
+<section class="homemenuiserie">
+	<div class="row expanded">
+		<div class="large-4 medium-12 small-12 columns menuiserie sectionmenuiserie">
+			<h2>Menuiserie</h2>
+			<h3>Menuiserie</h3>
+			<p>m est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l’imprimerie depuis les années 1500, quand un peintre an</p>
+			<a href="<?php the_permalink(); ?>" class="mylittlebutton">Je découvre</a>
+		</div>
+		<div class="large-4 medium-12 small-12 columns garagenord sectionmenuiserie">
+			<h2>Garage Nord</h2>
+			<h3>Garage Nord</h3>
+			<p>m est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l’imprimerie depuis les années 1500, quand un peintre an</p>
+			<a href="<?php the_permalink(); ?>" class="mylittlebutton">Je découvre</a>
+		</div>
+		<div class="large-4 medium-12 small-12 columns garagesud sectionmenuiserie">
+			<h2>Garage Sud</h2>
+			<h3>Garage Sud</h3>
+			<p>m est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l’imprimerie depuis les années 1500, quand un peintre an</p>
+			<a href="<?php the_permalink(); ?>" class="mylittlebutton">Je découvre</a>
+		</div>
+	</div>
+</section>
 
 
 <div class="row theformulaire">
